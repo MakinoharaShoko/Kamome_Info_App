72640133a84a84c9868b2a25dfdbda8f240e67f9
MakinoharaShoko <salufthansa@foxmail.com>
2021-12-18 22:47:38 +0800

    添加友链

--- src/Page/Links/Links.js
+++ src/Page/Links/Links.js
@@ -11,6 +11,9 @@ const Links = ()=>{
                 <a href="http://ylinknest.top/" target="_blank" rel="noopener">ylink’s Nest</a>
             </Paragraph>
             <Paragraph>
+                <a href="http://www.zhtsu.cn/" target="_blank" rel="noopener">zhisu</a>
+            </Paragraph>
+            <Paragraph>
                 ……本人乃社交恐惧症患者，不要指望这里有多少友链，如果想加的话可以联系我……
             </Paragraph>
         </Typography>
