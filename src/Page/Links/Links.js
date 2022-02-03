06e7ef7fc4a50452705ead93b20ee3ad11a674b7
MakinoharaShoko <salufthansa@foxmail.com>
2022-02-03 19:28:29 +0800

    Update Links.js

--- src/Page/Links/Links.js
+++ src/Page/Links/Links.js
@@ -14,6 +14,9 @@ const Links = ()=>{
                 <a href="http://www.zhtsu.cn/" target="_blank" rel="noopener">zhisu</a>
             </Paragraph>
             <Paragraph>
+                <a href="https://lzacg.us/" target="_blank" rel="noopener">量子 ACG</a>
+            </Paragraph>
+            <Paragraph>
                 ……本人乃社交恐惧症患者，不要指望这里有多少友链，如果想加的话可以联系我……
             </Paragraph>
         </Typography>
